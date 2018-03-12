6307ee16527f7231586cbc0395ca4b50b49ac5c5
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2018-03-12 15:24:41 +1100

    Rewrote the node

--- honda_controlling_scripts/scripts/can_listener_node_civic.py
+++ honda_controlling_scripts/scripts/can_listener_node_civic.py
@@ -4,70 +4,76 @@ from can_msgs.msg import Frame
 from geometry_msgs.msg import TwistStamped
 import cantools
 import math
+from rospkg import RosPack
 
-wheel_angle = 0
-DBW_mode = 0
-wheel_speed = 0
-wheel_angle_2 = 0
+"""
+Node that subscribes to the can_frame_msgs and extracts
+the information from the wheels and the steering wheel
+to compose a TwistStamped message.
 
-msg1_id = 464
-msg2_id = 330
-counter_msg1 = 0
-counter_msg2 = 0
-car_length = 2.1 # should be changed
-
-def callback(msg):
-    msgID = msg.id
-    if (msgID == msg1_id):
-        global counter_msg1
-        counter_msg1 = 1
-        data = can_db.decode_message(msgID, msg.data)
-        speed_RL = data['WHEEL_SPEED_FL']
-        speed_RR = data['WHEEL_SPEED_FR']
-        global wheel_speed
-        wheel_speed = (speed_RL+speed_RR)/2
+Author: Ahmed Radwan <ahmed.ali.radwan94 at gmail.com>
+Author: Sammy Pfeiffer <Sammy.Pfeiffer at student.uts.edu.au>
+"""
 
 
-    if (msgID == msg2_id):
-        global counter_msg2
-        counter_msg2 = 1
-        data = can_db.decode_message(msgID, msg.data)
-        global wheel_angle
-        wheel_angle = data['STEER_WHEEL_ANGLE']
+class TwistFromCan(object):
+    # Constants
+    WHEEL_SPEED_MSG_ID = 464
+    STEER_WHEEL_MSG_ID = 330
+    # should be changed, this is for the coffee car currently.
+    CAR_LENGTH = 2.1
+    KPH_TO_MS = 0.277778
+    STEER_WHEEL_ANGLE_TO_WHEEL_ANGLE = 50
+    MAX_WHEEL_ANGLE = 405.0
 
+    def __init__(self):
+        rospy.loginfo("Initializing TwistFromCan")
+        rp = RosPack()
+        pkg_path = rp.get_path('panda_bridge_ros')
+        path_to_dbc = pkg_path + '/config/honda_civic_touring_2016_can_for_cantools.dbc'
+        self.can_db = cantools.db.load_file(path_to_dbc)
+        self.wheel_speed = None
+        self.wheel_angle = None
+        self.sub = rospy.Subscriber("can_frame_msgs", Frame,
+                                    self.callback, queue_size=10)
+        self.pub = rospy.Publisher("twist_stamped",
+                                   TwistStamped, queue_size=10)
+        rospy.loginfo("Done")
 
+    def callback(self, msg):
+        msgID = msg.id
+        if (msgID == self.WHEEL_SPEED_MSG_ID):
+            data = self.can_db.decode_message(msgID, msg.data)
+            speed_RL = data['WHEEL_SPEED_FL']
+            speed_RR = data['WHEEL_SPEED_FR']
+            self.wheel_speed = (speed_RL + speed_RR) / 2.0
 
-    # else:
-        # print("nothing found!!")
+        if (msgID == self.STEER_WHEEL_MSG_ID):
+            data = self.can_db.decode_message(msgID, msg.data)
+            self.wheel_angle = data['STEER_WHEEL_ANGLE']
 
+    def run(self):
+        rospy.loginfo("Running!")
+        twist_msg = TwistStamped()
+        rate = rospy.Rate(10)
+        while not rospy.is_shutdown():
+            if self.wheel_speed and self.wheel_angle:
+                x = self.wheel_speed * self.KPH_TO_MS
+                self.wheel_angle = (
+                    self.wheel_angle * self.STEER_WHEEL_ANGLE_TO_WHEEL_ANGLE) / self.MAX_WHEEL_ANGLE
+                z = x * (math.tan(math.radians(self.wheel_angle)) /
+                         self.CAR_LENGTH)
 
-
-
+                twist_msg.twist.linear.x = x
+                twist_msg.twist.angular.z = z
+                print("Wheel speed: " + str(self.wheel_speed))
+                print("Wheel angle: " + str(self.wheel_angle))
+                print("z: " + str(twist_msg.twist.angular.z))
+                self.pub.publish(twist_msg)
+            rate.sleep()
 
 
 if __name__ == '__main__':
-    can_db = cantools.db.load_file('./dbc/honda_civic_touring_2016_can_for_cantools.dbc')
-
     rospy.init_node('listener', anonymous=True)
-
-    rospy.Subscriber("can_frame_msgs", Frame, callback)
-    pub = rospy.Publisher("twist_stamped", TwistStamped)
-    twist_msg = TwistStamped()
-    rate = rospy.Rate(10)
-    global car_length
-    while not rospy.is_shutdown():
-        if (counter_msg1 + counter_msg2 >= 2):
-            twist_msg.twist.linear.x = wheel_speed*0.277778
-            wheel_angle = (wheel_angle*50)/405.0
-            twist_msg.twist.angular.z = (twist_msg.twist.linear.x*(
-                math.tan((wheel_angle*math.pi)/180.0))/(car_length))
-            print("Wheel angle: " + str(wheel_angle))
-            print("z: " + str(twist_msg.twist.angular.z))
-            pub.publish(twist_msg)
-        if (counter_msg1 == 1):
-            print("msg1(391) only rec.")
-        if (counter_msg2 == 1):
-            print("msg2(387) only rec.")
-        else:
-            print("nothing rec.")
-        rate.sleep()
+    tfc = TwistFromCan()
+    tfc.run()
