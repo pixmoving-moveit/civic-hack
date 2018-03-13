be40184bd697cfb86c9b56cab8a3c5f75ca63e87
Sammy Pfeiffer <sammypfeiffer@gmail.com>
2018-03-13 16:28:16 +1100

    add joy to steer

---
+++ honda_controlling_scripts/scripts/joy_to_steer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from sensor_msgs.msg import Joy
+import rospy
+from std_msgs.msg import Int16
+
+
+class JoyToSteer(object):
+    def __init__(self):
+        self.pub = rospy.Publisher('/steering_amount', Int16, queue_size=1)
+        self.sub = rospy.Subscriber('/joy', Joy, self.joy_cb, queue_size=1)
+
+    def joy_cb(self, data):
+        left_right = data.axes[0]
+        amount_steer = int(left_right * 3840)
+        self.pub.publish(amount_steer)
+        print("amount_steer: " + str(amount_steer))
+
+
+if __name__ == '__main__':
+    rospy.init_node('from_joy_to_steer')
+    jts = JoyToSteer()
+    rospy.spin()
